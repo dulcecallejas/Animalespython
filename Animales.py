083a6a9793cc77923cde8fdac014476153890efc
dulcecallejas <98191749+dulcecallejas@users.noreply.github.com>
2022-03-05 21:59:50 -0600

    Create Animales.py
    Agrego archivo desde VS Code

---
+++ Animales.py
@@ -0,0 +1,33 @@
+print ("Welcome to de animal game")
+while True:
+  print ("1 Perro")
+  print ("2 Gato")
+  print ("3 Exit")
+
+  opcion = int(input("Introduce la opcion==>"))
+  if opcion == 1:
+    print("""
+    ,-.___,-.
+    \_/_ _\_/
+      )O_O(
+     { (_) }
+      `-^-' 
+    """ 
+    )
+  elif opcion == 2:
+    print("""
+    ┈┈╱╲┈┈┈ ╱╲┈┈╭━╮┈
+    ┈╱╱╲╲__╱╱╲╲┈╰╮┃┈
+    ┈▏┏┳╮┈╭┳┓▕┈┈┃┃┈
+    ┈▏╰┻┛▼┗┻╯▕┈┈┃┃┈
+    ┈╲┈┈╰┻╯┈┈╱▔▔┈┃┈
+    ┈┈╰━┳━━━╯┈┈┈┈┃┈
+    ┈┈┈┈┃┏┓┣━━┳┳┓┃┈
+    ┈┈┈┈┗┛┗┛┈┈┗┛┗┛┈2"""
+    )
+
+  elif opcion == 3:
+     print("Bye")
+     break
+  else:
+    print("Opción incorrecta")
